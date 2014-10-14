593d2baaad33b87bf00ff152f793f23330f0f75f
Aldyaka Mushofan <mushofan@users.noreply.github.com>
2014-10-15 06:02:23 +0700

    Update submit_comm.php

--- submit_comm.php
+++ submit_comm.php
@@ -15,15 +15,50 @@
 	
 	$res = mysqli_query($con, "SELECT * FROM komen WHERE ID=$id ORDER BY Waktu DESC");
 	while($tabel = mysqli_fetch_array($res)){
-		echo"
-			<li class='art-list-item'>
-				<div class='art-list-item-title-and-time'>
-					<h2 class='art-list-title'><a href=''>".$tabel['Nama']."</a></h2>
-					<div class='art-list-time'>2 menit lalu</div>
-				</div>
-				<p>".$tabel['Komentar']."&hellip;</p>
-			</li>
-		";
+			$selisih = time() - strtotime($tabel['Waktu']);
+		if($selisih > 3600){
+			$Jam = floor($selisih/3600);
+			if($Jam >= 24){
+				$Hari = $Jam - 24;
+				$Jam = $Jam - 24;
+				$selisih %= 3600;
+				$Menit = floor($selisih/60);
+				echo "
+					<li class='art-list-item'>
+						<div class='art-list-item-title-and-time'>
+							<h2 class='art-list-title'><a href=''>".$tabel['Nama']."</a></h2>
+							<div class='art-list-time'>{$Hari} hari {$Jam} jam {$Menit} menit</div>
+						</div>
+						<p>".$tabel['Komentar']."&hellip;</p>
+					</li>
+				";
+			}
+			else{
+				$selisih %= 3600;
+				$Menit = floor($selisih/60);
+				echo "
+					<li class='art-list-item'>
+						<div class='art-list-item-title-and-time'>
+							<h2 class='art-list-title'><a href=''>".$tabel['Nama']."</a></h2>
+							<div class='art-list-time'>{$Jam} jam {$Menit} menit</div>
+						</div>
+						<p>".$tabel['Komentar']."&hellip;</p>
+					</li>
+				";
+			}
+		}
+		else{
+			$Menit = floor ($selisih/60);
+			echo "
+					<li class='art-list-item'>
+						<div class='art-list-item-title-and-time'>
+							<h2 class='art-list-title'><a href=''>".$tabel['Nama']."</a></h2>
+							<div class='art-list-time'>{$Menit} menit</div>
+						</div>
+						<p>".$tabel['Komentar']."&hellip;</p>
+					</li>
+			";
+		}
 	}
 	
 	mysqli_close($con);
